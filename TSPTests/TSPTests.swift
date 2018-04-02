cf66f0bbeb6856145b87183d5d5f968d14f77482
djeetee@users.noreply.github.com <djeetee@users.noreply.github.com>
2018-04-02 12:06:35 -0400

    Implemented unit test function to test the init of City, Tour and distance calculations.

--- TSPTests/TSPTests.swift
+++ TSPTests/TSPTests.swift
@@ -21,6 +21,43 @@ class TSPTests: XCTestCase {
         super.tearDown()
     }
     
+    func testCityInit() {
+        let cityName = "Toronto"
+        let cityLong = 43.645471
+        let cityLat = -79.380567
+        
+        let city = City(cityName: cityName, cityLongitude: cityLong, citylatitude: cityLat)
+        XCTAssertTrue(city.name == cityName, "City Name is \(city.description())")
+    }
+    
+    func testCalculateDistance() {
+        let cityA = City(cityName: "Toronto", cityLongitude: 43.645471, citylatitude: -79.380567)
+        let cityB = City(cityName: "Montreal", cityLongitude: 52.636372, citylatitude: -92.986532)
+
+        let longDistance = abs(cityA.longitude - cityB.longitude)
+        let latDistance = abs(cityA.latitude - cityB.latitude)
+        
+        let distance = sqrt( (longDistance * longDistance) + (latDistance * latDistance) )
+
+        XCTAssert(cityA.distanceTo(city: cityB) == distance, "cityA.distance = \(cityA.distanceTo(city: cityB)), distance = \(distance)")
+    }
+    
+    
+    func testTourInit() {
+        let cityA = City(cityName: "Toronto", cityLongitude: 43.645471, citylatitude: -79.380567)
+        let cityB = City(cityName: "Montreal", cityLongitude: 52.636372, citylatitude: -92.986532)
+        let cityC = City(cityName: "Vancouver", cityLongitude: 44.673527, citylatitude: 66.821027)
+        
+        var cities = [City]()
+        cities.append(cityA)
+        cities.append(cityB)
+        cities.append(cityC)
+        XCTAssertTrue(cities.count == 4, "cities has \(cities[0].name + ", " + cities[1].name + ", " + cities[2].name)")
+        
+        let tour = Tour(withCities: cities)
+        XCTAssertTrue(tour.size() == 4, "Tour has \(tour.size()) cities and they are \(tour.description())")
+        
+    }
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
